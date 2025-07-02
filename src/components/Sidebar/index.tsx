eabfaec36169949ae102efd496009e73d4192019
Muhammad Noman <jawad.bdo99@gmail.com>
2025-07-02 14:33:45 +0500

    push code

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -17,7 +17,7 @@ const Sidebar: React.FC<Props> = ({ menus }) => {
     return (
         <aside className="lg:w-[200px] xl:w-[240px] 2xl:w-[300px] fixed inset-y-0 left-0 z-10 h-screen bg-[#1A2026]">
             <div className="flex items-center pl-10 lg:h-[4rem] 2xl:h-[4rem] mt-5">
-                <Link href="/dashboard">
+                <Link href="/">
                     <Image src="/images/logo.png" alt="logo" width={86} height={34} className="lg:w-[165px] xl:w-[170px]" />
                 </Link>
             </div>
@@ -34,7 +34,7 @@ const Sidebar: React.FC<Props> = ({ menus }) => {
                                     <li key={item.id}>
                                         <Link
                                             href={item.link || ""}
-                                            className={`flex items-center relative overflow-visible gap-3 p-2 font-medium text-black-500 text-[18px] capitalize hover:text-[#EF4444] ${isActive ? "text-[#EF4444]" : "text-white"
+                                            className={`flex items-center relative overflow-visible w-fit gap-3 p-2 font-medium text-black-500 text-[18px] capitalize hover:text-[#EF4444] ${isActive ? "text-[#EF4444]" : "text-white"
                                                 } transition-colors duration-300`}
                                         >
                                             {item.icon && (
